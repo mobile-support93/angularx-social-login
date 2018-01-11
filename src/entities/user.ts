ba84af53e34aad6833df38c81953c0d06a165dd6
mobile-support93 <mobile-support93@users.noreply.github.com>
2018-01-11 22:28:13 +0530

    Update user.ts

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,10 +1,12 @@
-export class SocialUser {
-  provider: string;
-  id: string;
-  email: string;
-  name: string;
-  photoUrl: string;
-  firstName: string;
-  lastName: string;
-  authToken: string;
+export declare class SocialUser {
+    provider: string;
+    id: string;
+    email: string;
+    name: string;
+    photoUrl: string;
+    firstName: string;
+    lastName: string;
+    authToken: string;
+    token: string;
 }
+
